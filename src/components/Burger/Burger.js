3a3b3092c48e2dcfe73d8ffab247801f17a1869f
viktar-b <viktar.beliakou@gmail.com>
2021-04-07 21:52:17 +0100

    burger component update

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,12 +2,33 @@ import React from 'react';
 import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
+//TODO revise map and reduce
 const burger = ( props ) => {
+    let transformedIngredient = Object.keys(props.ingredients)
+        .map(
+            igKey => {
+                return [...Array(props.ingredients[igKey])]
+                    .map( ( _ , index) => {
+                        return <BurgerIngredient 
+                            key={igKey + index} 
+                            type={igKey} 
+                            />
+                    })  
+            } 
+        ).reduce(
+            (arr, el) => {
+                return arr.concat(el)
+            } , [] 
+        ); 
+    console.log(transformedIngredient.length);
+    if (transformedIngredient.length === 0) {
+        transformedIngredient = <p>Please add ingredients</p>;
+    }
+
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredient}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );

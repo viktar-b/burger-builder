ad2fb4e7ca9f2c0ec6d78b0e310e3017418ace80
viktar-b <viktar.beliakou@gmail.com>
2021-04-08 17:57:32 +0100

    Price display Closes #10

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -61,6 +61,7 @@ class BurgerBuilder extends Component{
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
+                    price={this.state.totalPrice}
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
